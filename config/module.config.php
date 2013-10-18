6fedf18bd0c09255a26b0b7651ae13411a55e0a7
David Pozsar <david.pozsar@megaweb.hu>
2013-10-18 14:04:23 +0200

    first commit

---
+++ config/module.config.php
@@ -0,0 +1,186 @@
+<?php
+
+return array(
+    'factory' => array(
+        'Grid\Paragraph\Model\Paragraph\StructureFactory' => array(
+            'adapter' => array(
+                'contentList'   => 'Grid\ContentList\Model\Paragraph\Structure\ContentList',
+            ),
+        ),
+    ),
+    'modules' => array(
+        'Grid\Paragraph' => array(
+            'customizeMapForms' => array(
+                'contentList'   => array(
+                    'element'   => 'general',
+                    'leadImage' => 'image',
+                ),
+            ),
+        ),
+    ),
+    'form' => array(
+        'Grid\Paragraph\CreateWizard\Start' => array(
+            'elements'  => array(
+                'type'  => array(
+                    'spec'  => array(
+                        'options'   => array(
+                            'options'   => array(
+                                'basic' => array(
+                                    'options'   => array(
+                                        'contentList'   => 'paragraph.type.contentList',
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        'Grid\Paragraph\Meta\Edit' => array(
+            'fieldsets' => array(
+                'contentList' => array(
+                    'spec'    => array(
+                        'name'      => 'contentList',
+                        'options'   => array(
+                            'label'     => 'paragraph.type.contentList',
+                            'required'  => false,
+                        ),
+                        'elements'  => array(
+                            'name'  => array(
+                                'spec'  => array(
+                                    'type'      => 'Zork\Form\Element\Text',
+                                    'name'      => 'name',
+                                    'options'   => array(
+                                        'label'     => 'paragraph.form.abstract.name',
+                                        'required'  => false,
+                                    ),
+                                ),
+                            ),
+                            'mode'  => array(
+                                'spec'  => array(
+                                    'type'      => 'Zork\Form\Element\Select',
+                                    'name'      => 'mode',
+                                    'options'   => array(
+                                        'label'     => 'contentList.paragraph.form.mode',
+                                        'required'  => false,
+                                        'options'   => array(
+                                            'some'  => 'contentList.paragraph.form.mode.some',
+                                            'all'   => 'contentList.paragraph.form.mode.all',
+                                        ),
+                                    ),
+                                ),
+                            ),
+                            'itemCount' => array(
+                                'spec'  => array(
+                                    'type'      => 'Zork\Form\Element\Number',
+                                    'name'      => 'itemCount',
+                                    'options'   => array(
+                                        'label'     => 'contentList.paragraph.form.itemCount',
+                                        'required'  => false,
+                                    ),
+                                    'attributes'    => array(
+                                        'value'     => 10,
+                                        'min'       => 1,
+                                    ),
+                                ),
+                            ),
+                            'displayUser' => array(
+                                'spec' => array(
+                                    'type'      => 'Zork\Form\Element\Checkbox',
+                                    'name'      => 'displayUser',
+                                    'options'   => array(
+                                        'label'     => 'contentList.paragraph.form.displayUser',
+                                        'required'  => false,
+                                    ),
+                                    'attributes'    => array(
+                                        'checked'   => true,
+                                    ),
+                                ),
+                            ),
+                            'displayCreated' => array(
+                                'spec' => array(
+                                    'type'      => 'Zork\Form\Element\Checkbox',
+                                    'name'      => 'displayCreated',
+                                    'options'   => array(
+                                        'label'     => 'contentList.paragraph.form.displayCreated',
+                                        'required'  => false,
+                                    ),
+                                    'attributes'    => array(
+                                        'checked'   => true,
+                                    ),
+                                ),
+                            ),
+                            'displayLeadImage' => array(
+                                'spec' => array(
+                                    'type'      => 'Zork\Form\Element\Checkbox',
+                                    'name'      => 'displayLeadImage',
+                                    'options'   => array(
+                                        'label'     => 'contentList.paragraph.form.displayLeadImage',
+                                        'required'  => false,
+                                    ),
+                                    'attributes'    => array(
+                                        'checked'   => true,
+                                    ),
+                                ),
+                            ),
+                            'displayLeadText' => array(
+                                'spec' => array(
+                                    'type'      => 'Zork\Form\Element\Checkbox',
+                                    'name'      => 'displayLeadText',
+                                    'options'   => array(
+                                        'label'     => 'contentList.paragraph.form.displayLeadText',
+                                        'required'  => false,
+                                    ),
+                                    'attributes'    => array(
+                                        'checked'   => true,
+                                    ),
+                                ),
+                            ),
+                            'displayReadMore' => array(
+                                'spec' => array(
+                                    'type'      => 'Zork\Form\Element\Checkbox',
+                                    'name'      => 'displayReadMore',
+                                    'options'   => array(
+                                        'label'     => 'contentList.paragraph.form.displayReadMore',
+                                        'required'  => false,
+                                    ),
+                                    'attributes'    => array(
+                                        'checked'   => true,
+                                    ),
+                                ),
+                            ),
+                            'tags' => array(
+                                'spec' => array(
+                                    'type'      => 'Grid\Tag\Form\Element\TagList',
+                                    'name'      => 'tags',
+                                    'options'   => array(
+                                        'label'     => 'contentList.paragraph.form.tags',
+                                        'required'  => false,
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'translator' => array(
+        'translation_file_patterns' => array(
+            'tag' => array(
+                'type'          => 'phpArray',
+                'base_dir'      => __DIR__ . '/../languages/contentList',
+                'pattern'       => '%s.php',
+                'text_domain'   => 'contentList',
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_map' => array(
+            'grid/paragraph/render/contentList'  => __DIR__ . '/../view/grid/paragraph/render/contentList.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
---
+++ languages/contentList/en.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'contentList.paragraph.form.mode' => 'Mode',
+    'contentList.paragraph.form.mode.some' => 'Some tags must match',
+    'contentList.paragraph.form.mode.all' => 'All tags must match',
+    'contentList.paragraph.form.itemCount' => 'Item count per page',
+    'contentList.paragraph.form.displayUser' => 'Display editor',
+    'contentList.paragraph.form.displayCreated' => 'Display created date',
+    'contentList.paragraph.form.displayLeadImage' => 'Display lead image',
+    'contentList.paragraph.form.displayLeadText' => 'Display lead text',
+    'contentList.paragraph.form.displayReadMore' => 'Display "read more" link',
+    'contentList.paragraph.form.tags' => 'Tags',
+    'contentList.paragraph.tagsLabel' => 'Tags',
+);
