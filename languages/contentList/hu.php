9ab3986064f942321ee5bc9f7306db7ff365b44f
Gridguyz Dev Platform <dev-platform@gridguyz.com>
2013-12-13 12:11:06 +0000

    Translations updated

---
+++ languages/contentList/hu.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+  'contentList.paragraph.form.displayCreated' => 'Készítési dátum megjelenítése',
+  'contentList.paragraph.form.displayLeadImage' => 'Bevezető kép megjelenítése',
+  'contentList.paragraph.form.displayLeadText' => 'Bevezető szöveg megjelenítése',
+  'contentList.paragraph.form.displayReadMore' => '"Tovább" hivatkozás megjelenítése',
+  'contentList.paragraph.tagsLabel' => 'Cimkék',
+);
