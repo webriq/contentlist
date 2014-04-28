24b433279c60c5a3b6b6c837704b2f81c49c7f8a
Gridguyz Dev Platform <dev-platform@gridguyz.com>
2014-04-28 08:34:47 +0000

    Translations updated

--- languages/contentList/hu.php
+++ languages/contentList/hu.php
@@ -4,5 +4,6 @@ return array (
   'contentList.paragraph.form.displayLeadImage' => 'Bevezető kép megjelenítése',
   'contentList.paragraph.form.displayLeadText' => 'Bevezető szöveg megjelenítése',
   'contentList.paragraph.form.displayReadMore' => '"Tovább" hivatkozás megjelenítése',
+  'contentList.paragraph.moreEntries' => 'Több bejegyzés',
   'contentList.paragraph.tagsLabel' => 'Címkék',
 );
